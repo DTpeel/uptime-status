fd79d13d8f91a61d3870e764943f821b8fe353fc
chenskiro <chenskiro@outlook.com>
2023-02-11 18:44:10 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,15 +1,12 @@
 window.Config = {
 
   // 显示标题
-  SiteName: 'Public Status',
+  SiteName: 'chenskiro 网站状态监控',
 
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'u2014651-452cbac1577fb4f94de47a6c',
   ],
 
   // 日志天数
@@ -21,16 +18,16 @@ window.Config = {
   // 导航栏菜单
   Navi: [
     {
-      text: 'Homepage',
-      url: 'https://status.org.cn/'
+      text: 'Blog',
+      url: 'https://chenskiro.com/'
     },
     {
       text: 'GitHub',
-      url: 'https://github.com/yb/uptime-status'
+      url: 'https://github.com/DTpeel/uptime-status'
     },
     {
-      text: 'Blog',
-      url: 'https://abo.xyz/'
+      text: 'Bangumi',
+      url: 'https://bgm.tv/user/chenskiro'
     },
   ],
 };
